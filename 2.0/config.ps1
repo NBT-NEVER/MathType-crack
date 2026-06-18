22ee42d10c8357256d99a26ef504226474085f7a
NBT-NEVER <1803040360@qq.com>
2026-06-18 10:24:35 +0800

    Add mathtype6.9 of network disconnection

--- 2.0/config.ps1
+++ 2.0/config.ps1
@@ -1,6 +1,6 @@
 ﻿# MathType Crack Configuration
 
-$mathtypePath = "C:\D VOLUME\APP\STUDY_PIRATED\mathtype\MathType.exe"
+$mathtypePath = "D:\Lenovo\Mathtype\MathType.exe"
 $registryBase = "HKCU\Software\Install Options"
 $registryKey = "Options6.9"
 $processName = "MathType"
